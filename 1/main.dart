eaf1b3d8932a22d79b8de31ae7bc2c0e02dc407d
ndelanou <nicolas.delanou@shipfix.com>
2022-12-01 09:08:28 +0100

    challenge 1

---
+++ 1/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'dart:math';
+
+const filename = 'input.txt';
+void main(List<String> args) {
+
+  final file = File(filename);
+  final lines = file.readAsStringSync();
+  final elfs = lines.split('\n\n');
+
+  print('lines: ${lines.length}');
+  print('elfs: ${elfs.length}');
+
+  final elfsCals = elfs.map(
+    (e) => e
+      .split('\n')
+      .map((calStr) => int.parse(calStr))
+      .fold(0, (acc, cal) => acc + cal)
+  );
+
+  // Part 1
+  final maxCal = elfsCals.reduce(max);
+  print('max: $maxCal');
+
+  // Part 2
+  final sortedCals = (elfsCals.toList()..sort()).reversed.toList();
+  final top3Sum = sortedCals[0] + sortedCals[1] + sortedCals[2];
+  print('top 3 sum: $top3Sum');
+
+}
