c8b071b45f28d9ccb71b92947ce2e7e098b04677
ndelanou <nicolas.delanou@shipfix.com>
2022-12-01 13:53:28 +0100

    make file read async + dart format

--- 1/main.dart
+++ 1/main.dart
@@ -2,20 +2,19 @@ import 'dart:io';
 import 'dart:math';
 
 const filename = 'input.txt';
-void main(List<String> args) {
-
+void main(List<String> args) async {
   final file = File(filename);
-  final lines = file.readAsStringSync();
-  final elfs = lines.split('\n\n');
-
+  final lines = await file.readAsString();
   print('lines: ${lines.length}');
+
+  final elfs = lines.split('\n\n');
   print('elfs: ${elfs.length}');
 
   final elfsCals = elfs.map(
     (e) => e
-      .split('\n')
-      .map((calStr) => int.parse(calStr))
-      .fold(0, (acc, cal) => acc + cal)
+        .split('\n')
+        .map((calStr) => int.parse(calStr))
+        .fold(0, (acc, cal) => acc + cal),
   );
 
   // Part 1
@@ -26,5 +25,4 @@ void main(List<String> args) {
   final sortedCals = (elfsCals.toList()..sort()).reversed.toList();
   final top3Sum = sortedCals[0] + sortedCals[1] + sortedCals[2];
   print('top 3 sum: $top3Sum');
-
 }
