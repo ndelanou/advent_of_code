5ddbff99eba6fa616761e307ab72545ad6272a22
ndelanou <nicolas.delanou@shipfix.com>
2022-12-09 00:58:56 +0100

    delete unused files

--- lib/src/2022/2022.dart
+++ lib/src/2022/2022.dart
@@ -1,1 +1,2 @@
 export 'day08.dart';
+export 'day09.dart';
---
+++ lib/src/2022/day09.dart
@@ -0,0 +1,21 @@
+import '../utils/utils.dart';
+
+class Day09 extends GenericDay {
+  Day09() : super(2022, 9);
+
+  @override
+  parseInput() {
+
+  }
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
+
